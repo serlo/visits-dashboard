9d7e4cf968bf900992d34a37ba685bbf3ca4ae38
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-10-04 17:47:19 +0200

    cache alias

--- src/enrich_rankings.js
+++ src/enrich_rankings.js
@@ -1,18 +1,33 @@
 const fs = require('fs')
 const rankings = require('../intermediate/rankings.json')
 
+let cache = {}
+
+if (fs.existsSync('./alias_cache.json')) {
+  cache = JSON.parse(fs.readFileSync('./alias_cache.json', 'utf-8'))
+}
+
 run()
 
 async function run() {
   for (const entry of rankings.topWinners.concat(rankings.topLosers)) {
-    const res = await fetch('https://de.serlo.org/' + entry.id)
-    const url = res.url.replace('https://de.serlo.org', '')
-    entry.alias = url
-    console.log(entry.id, entry.alias)
+    if (cache[entry.id]) {
+      entry.alias = cache[entry.id]
+    } else {
+      const res = await fetch('https://de.serlo.org/' + entry.id)
+      const url = res.url.replace('https://de.serlo.org', '')
+      entry.alias = url
+      console.log('fetching', entry.id, entry.alias)
+      cache[entry.id] = url
+    }
   }
   fs.writeFileSync(
     './intermediate/rankings.json',
     JSON.stringify(rankings, null, 2)
+  )
+  fs.writeFileSync(
+    './intermediate/alias_cache.json',
+    JSON.stringify(cache, null, 2)
   )
   process.exit()
 }
